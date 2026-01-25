92ac7848e2722944d066d101decf9fbc47a64466
amitbishnoii <amitbishnoi29012@gmail.com>
2026-01-25 18:32:42 +0530

    solve leetcode 1480

---
+++ arrays/LeetCode_1480.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class LeetCode_1480 {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4 };
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                continue;
+            }
+            nums[i] = nums[i] + nums[i-1];
+        }
+        System.out.println(Arrays.toString(nums));
+        
+    }
+}
