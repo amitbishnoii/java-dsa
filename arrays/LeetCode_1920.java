59ea086009791147bba600e571d28bc0e66f27a9
amitbishnoii <amitbishnoi29012@gmail.com>
2026-01-26 10:41:46 +0530

    solve leetcode 1920

---
+++ arrays/LeetCode_1920.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class LeetCode_1920 {
+    public static void main(String[] args) {
+        int[] nums = { 5, 0, 1, 2, 3, 4 };
+        System.out.println(build_permutation(nums));
+    }
+
+    static int[] build_permutation(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
