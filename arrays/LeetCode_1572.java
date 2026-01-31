f90c2a79381797da9142d1f05acc8b61f54c4a9b
amitbishnoii <amitbishnoi29012@gmail.com>
2026-01-31 10:52:48 +0530

    solve leetcode 1572

---
+++ arrays/LeetCode_1572.java
@@ -0,0 +1,18 @@
+public class LeetCode_1572 {
+    public static void main(String[] args) {
+        int[][] mat = {
+                { 1, 1, 1, 1 },
+                { 1, 1, 1, 1 },
+                { 1, 1, 1, 1 }
+        };
+        int sum = 0;
+        for (int row = 0; row < mat.length; row++) {
+            for (int col = 0; col < mat[row].length; col++) {
+                if (row == col || row + col == mat.length - 1) {
+                    sum += mat[row][col];
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
