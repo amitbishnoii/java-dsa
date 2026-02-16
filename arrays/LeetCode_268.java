b06c12d582df3aa1e4b790f4e3d073aa1a1f8af6
amitbishnoii <amitbishnoi29012@gmail.com>
2026-02-16 17:16:58 +0530

    solve leetcode 268

---
+++ arrays/LeetCode_268.java
@@ -0,0 +1,16 @@
+public class LeetCode_268 {
+    public static void main(String[] args) {
+        int[] arr = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+        System.out.println(missingNumber(arr));
+    }
+
+    static int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = (n * (n + 1)) / 2;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            temp += nums[i];
+        }
+        return sum - temp;
+    }
+}
