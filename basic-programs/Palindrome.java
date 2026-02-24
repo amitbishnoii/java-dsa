29794da3897d159eae0ed178dd0f3e6f2ffb1e06
amitbishnoii <amitbishnoi29012@gmail.com>
2026-02-24 13:29:28 +0530

    solve leetcode 9

---
+++ basic-programs/Palindrome.java
@@ -0,0 +1,18 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println(palindrome(151));
+    }
+
+    static boolean palindrome(int x) {
+        int a = x;
+        int rev = 0;
+        while (x > 0) {
+            int temp = x % 10;
+            rev = (rev * 10) + temp;
+            x = x / 10;
+        }
+        System.out.println(rev);
+        System.out.println(x);
+        return rev == a;
+    }
+}
