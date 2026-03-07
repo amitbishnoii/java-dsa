4aaac7e0580a4eb0662a8a534fe2e4a51c0ce8c0
amitbishnoii <amitbishnoi29012@gmail.com>
2026-03-07 16:51:54 +0530

    solve leetcode 75

---
+++ two-pointers/DutchFlag.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class DutchFlag {
+    public static void main(String[] args) {
+        int[] nums = { 2, 0, 2, 1, 1, 0 };
+        sortColors(nums);
+    }
+
+    static void sortColors(int[] nums) {
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+
+        for (int i : nums) {
+            if (i == 0) {
+                zero += 1;
+            }
+            else if (i == 1) {
+                one += 1;
+            }
+            else {
+                two += 1;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (zero != 0) {
+                nums[i] = 0;
+                zero--;
+            }
+            else if (zero == 0 && one != 0) {
+                nums[i] = 1;
+                one--;
+            }
+            else if (zero == 0 && one == 0 && two != 0) {
+                nums[i] = 2;
+                two--;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
