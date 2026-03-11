f19f3d100abee0ccb25e68a1dbce830b621365ed
amitbishnoii <amitbishnoi29012@gmail.com>
2026-03-11 18:10:31 +0530

    longest substring with atleast k distinct

---
+++ sliding-window/longestSubstring.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class longestSubstring {
+    // https://www.geeksforgeeks.org/problems/longest-k-unique-characters-substring0853/1
+
+    public static void main(String[] args) {
+        String s = "aabacbebebe";
+        int k = 3;
+        System.out.println(longestKSubstr(s, k));
+    }
+
+    static int longestKSubstr(String s, int k) {
+        HashMap<Character, Integer> charMap = new HashMap<>();
+        int low = 0;
+        int result = 0;
+        for (int high = 0; high < s.length(); high++) {
+            charMap.put(s.charAt(high), charMap.getOrDefault(s.charAt(high), 0) + 1);
+            while (charMap.size() > k) {
+                charMap.put(s.charAt(low), charMap.get(s.charAt(low)) - 1);
+                if (charMap.get(s.charAt(low)) == 0) {
+                    charMap.remove(s.charAt(low));
+                }
+                low++;
+            }
+            if (charMap.size() == k) {
+                int len = (high - low) + 1;
+                result = Math.max(result, len);
+            }
+        }
+
+        return result;
+    }
+}
