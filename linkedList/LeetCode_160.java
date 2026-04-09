956cd72fe2ff032ea870d75c6835c9dd39b5570c
amitbishnoii <amitbishnoi29012@gmail.com>
2026-04-09 13:41:00 +0530

    solve leetcode 160

---
+++ linkedList/LeetCode_160.java
@@ -0,0 +1,13 @@
+public class LeetCode_160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+            a = (a == null) ? headB : a.next;
+            b = (b == null) ? headA : b.next;
+        }
+
+        return a;
+    }
+}
